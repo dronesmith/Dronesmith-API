b071705febbae4d4dde29b46ef5b47940f68c085
flatlevel <geoff@skyworksas.com>
2016-10-10 18:29:29 -0700

    API Core: Initial work on RT port. Basic session handling complete.

--- src/dronemanager/dronemanager.go
+++ src/dronemanager/dronemanager.go
@@ -5,22 +5,56 @@ import (
   "os"
   "strconv"
   "net"
+  "rest"
   "time"
   "math/rand"
+  "utils/keen"
+  "sync"
   "dronemanager/dronedp"
 )
 
+const (
+  KEEN_ENV = "testing"
+  KEEN_WRITE = "7215fb73a83d556fed9ff4c12b057aec30cdcb75735d56b736f41b74c434fa964fefda477fde39953a6fa6dd095f4fcf3bce1b1d59731c640c9800890bc7a137c242f62361d84d3aa57f2e95009209f921ec65d4130f0bbb838c262b330f3767"
+  KEEN_ID = "57a4c674e86170469d49e265"
+)
+
+var (
+  SessionIds uint32 = 1
+)
+
+type DLTracker struct {
+  Env string
+  Event string
+  Session Session
+}
+
 type DroneManager struct {
   port uint
-  sessions map[string]Session
+  keenBatch *keen.BatchClient
+  sessions map[uint32]*Session
+  sessionLock sync.RWMutex
+  conn *net.UDPConn
 }
 
 type Session struct {
-  id    uint32
+  id      uint32
+  State   string
+  Drone   map[string]interface{}
+  User    string
+  lastUpdate time.Time
 }
 
 func (s *Session) genRandomId() {
   s.id = uint32(rand.Int())
+}
+
+func (s *Session) genId() {
+  s.id = SessionIds
+  SessionIds++
+  if SessionIds == 0 {
+    SessionIds = 1
+  }
 }
 
 func CheckError(err error) {
@@ -32,9 +66,32 @@ func CheckError(err error) {
 
 func NewDroneManager(port uint) *DroneManager {
   rand.Seed(time.Now().UTC().UnixNano())
+  keenClient := &keen.Client{WriteKey: KEEN_WRITE, ProjectID: KEEN_ID}
   return &DroneManager{
     port,
-    make(map[string]Session),
+    keen.NewBatchClient(keenClient, 10 * time.Second),
+    make(map[uint32]*Session),
+    sync.RWMutex{},
+    nil,
+  }
+}
+
+func (m *DroneManager) checkTimers() {
+  for {
+    m.sessionLock.Lock()
+    for id, sess := range m.sessions {
+      if time.Now().Sub(sess.lastUpdate) > (5 * time.Second) {
+        log.Println(id, "Session timeout")
+
+        delete(m.sessions, id)
+        m.keenBatch.AddEvent("dronelink", &DLTracker{
+          Env: KEEN_ENV,
+          Event: "disconnect",
+        })
+      }
+    }
+    m.sessionLock.Unlock()
+    time.Sleep(5 * time.Second)
   }
 }
 
@@ -42,19 +99,21 @@ func (m *DroneManager) Listen() {
   ServerAddr,err := net.ResolveUDPAddr("udp", ":" + strconv.Itoa(int(m.port)))
   CheckError(err)
 
-  ServerConn, err := net.ListenUDP("udp", ServerAddr)
+  m.conn, err = net.ListenUDP("udp", ServerAddr)
   CheckError(err)
-  defer ServerConn.Close()
+  defer m.conn.Close()
 
   if err != nil {
-	   log.Println(err)
-     panic(err)
+	  log.Println(err)
+    panic(err)
   }
 
   buf := make([]byte, 1024)
 
+  go m.checkTimers()
+
   for {
-    n,addr,err := ServerConn.ReadFromUDP(buf)
+    n,addr,err := m.conn.ReadFromUDP(buf)
     if err != nil {
       log.Println("Error: ",err)
     }
@@ -62,12 +121,87 @@ func (m *DroneManager) Listen() {
     if decoded, err := dronedp.ParseMsg(buf[0:n]); err != nil {
       log.Println(err)
     } else {
-      log.Println(addr)
-      m.handleMessage(decoded)
+      // Doing this async
+      go m.handleMessage(decoded, addr)
     }
   }
 }
 
-func (m *DroneManager) handleMessage(decoded *dronedp.Msg) {
-  log.Println(decoded)
+func (m *DroneManager) handleMessage(decoded *dronedp.Msg, addr *net.UDPAddr) {
+  // log.Println(decoded)
+
+  switch decoded.Op {
+  case dronedp.OP_STATUS:
+    statusMsg := decoded.Data.(*dronedp.StatusMsg)
+    m.handleStatusMessage(statusMsg, addr, decoded.Session)
+  }
+}
+
+func (m *DroneManager) handleStatusMessage(msg *dronedp.StatusMsg, addr *net.UDPAddr, session uint32) {
+  switch msg.Op {
+  case "connect": m.handleStatusConnect(msg, addr)
+  case "status": m.handleStatusUpdate(msg, addr, session)
+  }
+}
+
+func (m *DroneManager) handleStatusConnect(msg *dronedp.StatusMsg, addr *net.UDPAddr) {
+  if resp, err := rest.RequestDroneInfo(msg.Serial, msg.Email, msg.Password); err != nil {
+    log.Println("Auth failed:", err)
+  } else {
+
+    userId := resp.User["_id"].(string)
+
+    sessObj := &Session{
+      State: "online",
+      Drone: resp.Drone,
+      User: userId,
+      lastUpdate: time.Now(),
+    }
+
+    sessObj.genId()
+
+    m.sessionLock.Lock()
+    defer m.sessionLock.Unlock()
+    m.sessions[sessObj.id] = sessObj
+
+    // record keen event
+    m.keenBatch.AddEvent("dronelink", &DLTracker{
+      Env: KEEN_ENV,
+      Event: "connect",
+      Session: *sessObj,
+    })
+
+    if msg, err := dronedp.GenerateMsg(dronedp.OP_STATUS, sessObj.id, sessObj); err != nil {
+      log.Println("Could not build D2P MSG:", err)
+    } else {
+      if _, err = m.conn.WriteToUDP(msg, addr); err != nil {
+        log.Println("Network error:", err)
+      } else {
+        log.Println("Session Changed:", sessObj.id)
+      }
+    }
+  }
+}
+
+func (m *DroneManager) handleStatusUpdate(msg *dronedp.StatusMsg, addr *net.UDPAddr, id uint32) {
+
+  m.sessionLock.Lock()
+  defer m.sessionLock.Unlock()
+  if _, found := m.sessions[id]; found {
+    // make sure this is ref so we update the timestamp.
+    sessObj := m.sessions[id]
+    sessObj.lastUpdate = time.Now()
+
+    if msg, err := dronedp.GenerateMsg(dronedp.OP_STATUS, sessObj.id, sessObj); err != nil {
+      log.Println("Could not build D2P MSG:", err)
+    } else {
+      if _, err = m.conn.WriteToUDP(msg, addr); err != nil {
+        log.Println("Network error:", err)
+      }
+    }
+
+    // Get payload data if any (TODO)
+
+    // Make command buffer null (TODO)
+  }
 }
--- src/rest/rest.go
+++ src/rest/rest.go
@@ -6,13 +6,16 @@
 package rest
 
 import (
+  "fmt"
+  "bytes"
   "log"
   "net/http"
   "strconv"
+  "encoding/json"
 )
 
 const (
-
+  CLOUD_ADDR = "localhost:4000"
 )
 
 var (
@@ -36,4 +39,55 @@ func (r *RestServer) Listen() {
 }
 
 func (r *RestServer) rootHandler(w http.ResponseWriter, req* http.Request) {
+}
+
+//
+// Static method. Asks the Cloud for drone information.
+// Returns a map with the response data if good, error if bad response.
+//
+type UserDroneInfoRes struct {
+  Status  string `json:"status"`
+  User    map[string]interface{} `json:"user"`
+  Drone   map[string]interface{} `json:"drone"`
+}
+
+func RequestDroneInfo(serial, user, pass string) (*UserDroneInfoRes, error) {
+  postData := map[string]string {
+    "serialId": serial,
+    "email": user,
+    "password": pass,
+  }
+  buf, _ := json.Marshal(postData)
+  resp, err := http.Post("http://" + CLOUD_ADDR + "/rt/droneinfo", "application/json", bytes.NewBuffer(buf))
+
+  if err != nil {
+    return nil, err
+  }
+
+  switch resp.StatusCode {
+  case 200:
+    decoder := json.NewDecoder(resp.Body)
+    t := UserDroneInfoRes{}
+    err := decoder.Decode(&t)
+    if err != nil {
+      return nil, err
+    }
+    defer resp.Body.Close()
+
+    return &t, nil
+  default: // anything but 200
+    decoder := json.NewDecoder(resp.Body)
+    t := map[string]string {
+      "error": "",
+    }
+    err := decoder.Decode(&t)
+    if err != nil {
+        return nil, err
+    }
+    defer resp.Body.Close()
+
+    return nil, fmt.Errorf(t["error"])
+  }
+
+  return nil, err
 }
